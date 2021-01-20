c500e019df8a8263ea652b1203dfbaab8ae27e78
Iryna <ineklesa@gmail.com>
2021-01-20 16:02:27 +0200

    Added 'Page not found' and 'Services page', header in process

--- js/index.js
+++ js/index.js
@@ -1,24 +1,19 @@
-$(document).ready(function(){
-    $('.about__carousell').slick( {
-        dots: true,
-        infinite: true,
-        arrows: false,
-        autoplay: true
-    });
+$(document).ready(function () {
+  $('.about__carousell').slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
   });
 
-
-  $(document).ready(function(){
-    $('.popular__list').slick( {
-        dots: true,
-        arrows: false,
-        mobileFirst: true,
-        responsive: [
-           {
-              breakpoint: 767,
-              settings: "unslick"
-           }
-        ]
-    });
+  $('.popular__list').slick({
+    dots: true,
+    arrows: false,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: 'unslick',
+      },
+    ],
   });
-
+});
