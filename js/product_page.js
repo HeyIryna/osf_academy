817827fd0791c0c029bc626f15aef628449853bf
Iryna <ineklesa@gmail.com>
2021-01-27 12:16:08 +0200

    Added product tabs toggle functionality

--- js/product_page.js
+++ js/product_page.js
@@ -1,28 +1,37 @@
 $(document).ready(function () {
-    $('.product__images').slick({
-        dots: true,
-        infinite: true,
-        mobileFirst: true,
-        arrows: false,
-        infinite: true,
-        slidesToShow: 3,
-        responsive: [
-            {
-              breakpoint: 767,
-              settings: 'unslick',
-            },
-          ]
-      });
-  
-    $('.popular__list').slick({
-      dots: true,
-      arrows: false,
-      mobileFirst: true,
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: 'unslick'
-        },
-      ],
-    });
+  $('.product__images').slick({
+    dots: true,
+    infinite: true,
+    mobileFirst: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: 'unslick',
+      },
+    ],
   });
+
+  $('.popular__list').slick({
+    dots: true,
+    arrows: false,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: 'unslick',
+      },
+    ],
+  });
+});
+
+// Product tabs toggle
+$('.tabs__item').click(function () {
+  let name = $(this).attr('data-tab');
+  let content = $('.tabs__info-item[data-tab="' + name + '"]');
+
+  $(this).toggleClass('active').siblings().removeClass('active');
+  content.toggleClass('active').siblings().removeClass('active');
+});
